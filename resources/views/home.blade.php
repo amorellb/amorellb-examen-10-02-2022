73189e5ce3052e9c63276a8f7a4938dd8df251f8
amorellb <amorellb@cifpfbmoll.eu>
2022-02-10 18:38:31 +0100

    feat: add home view, and navigation and plantilla layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,11 @@
-<?php
+@include('layouts.plantilla')
+
+<main>
+    <h1 class="text-2xl text-center mt-5">Uep!
+        @if (Route::has('login'))
+            @auth
+                <span>{{ ucfirst(Auth::user()->name) }}</span>
+            @endauth
+        @endif
+    </h1>
+</main>
