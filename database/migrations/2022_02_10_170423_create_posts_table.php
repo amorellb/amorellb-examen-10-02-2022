46d7a794da61becbf57ebecb642294becaff5710
amorellb <amorellb@cifpfbmoll.eu>
2022-02-10 18:13:03 +0100

    feat: add posts migration

---
+++ database/migrations/2022_02_10_170423_create_posts_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('title')->unique();
+            $table->string('summary')->nullable();
+            $table->string('content')->nullable();
+            $table->string('expiry')->nullable();
+            $table->string('commentable')->nullable();
+            $table->string('access')->nullable();
+        });
+
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')
+                ->nullable()
+                ->after('id');
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
