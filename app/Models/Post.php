4bd98d1f5be57f11c76bbe82d866b9bc1f0bfe7b
amorellb <amorellb@cifpfbmoll.eu>
2022-02-10 18:27:22 +0100

    feat: add controller and post model

---
+++ app/Models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    public $table = 'contacts';
+
+    protected $fillable = ['title', 'summary', 'content', 'expiry', 'commentable', 'access', 'user_id'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
