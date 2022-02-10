bbfa55918b41db1f196160331ac87944c608421f
amorellb <amorellb@cifpfbmoll.eu>
2022-02-10 18:21:17 +0100

    feat: add to users to the database one admin and one user

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            ['name' => 'Bernat', 'email' => 'bernat@email.com',
+                'password' => Hash::make('12345678'), 'role' => 'admin'],
+            ['name' => 'Margalida', 'email' => 'margalida@email.com',
+                'password' => Hash::make('12345678'), 'role' => 'admin'],
+        ];
+        DB::table('users')->insert($users);
+    }
+}
